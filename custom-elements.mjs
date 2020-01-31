79fa89dd74debaeacbe3d6e06321bec99187baba
dev.mohe <Moritz.Hedtke@t-online.de>
2020-01-31 16:24:55 +0100

    Initial styling implementation

--- custom-elements.mjs
+++ custom-elements.mjs
@@ -8,7 +8,19 @@ export class Button extends HTMLElement {
 
         var button = document.createElement('button')
         button.setAttribute('type', 'button')
-        button.setAttribute('class', 'btn btn-primary')
+
+        var buttonStyle = 'primary' // default
+        buttonStyle = this.hasAttribute('primary') ? 'primary' : buttonStyle;
+        buttonStyle = this.hasAttribute('secondary') ? 'secondary' : buttonStyle;
+        buttonStyle = this.hasAttribute('success') ? 'success' : buttonStyle;
+        buttonStyle = this.hasAttribute('danger') ? 'danger' : buttonStyle;
+        buttonStyle = this.hasAttribute('warning') ? 'warning' : buttonStyle;
+        buttonStyle = this.hasAttribute('info') ? 'info' : buttonStyle;
+        buttonStyle = this.hasAttribute('light') ? 'light' : buttonStyle;
+        buttonStyle = this.hasAttribute('dark') ? 'dark' : buttonStyle;
+        buttonStyle = this.hasAttribute('link') ? 'link' : buttonStyle;
+
+        button.setAttribute('class', `btn btn-${buttonStyle}`)
         button.textContent = this.textContent;
 
         const linkElem = document.createElement('link');
