7d6d90bb33e5af451b2cfdd216e1b9a0918d9ac5
dev.mohe <Moritz.Hedtke@t-online.de>
2020-02-01 15:21:22 +0100

    Test

---
+++ server.mjs
@@ -0,0 +1,34 @@
+const http = require('http');
+const WebSocket = require('ws');
+ 
+const server = http.createServer();
+const wss = new WebSocket.Server({ noServer: true });
+ 
+wss.on('connection', function connection(ws, request, client) {
+    const ip = request.connection.remoteAddress;
+    ws.on('message', function message(msg) {
+    console.log(`Received message ${msg} from user ${client}`);
+  
+    wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      });
+
+    });
+});
+ 
+server.on('upgrade', function upgrade(request, socket, head) {
+  authenticate(request, (err, client) => {
+    if (err || !client) {
+      socket.destroy();
+      return;
+    }
+ 
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request, client);
+    });
+  });
+});
+ 
+server.listen(8080);
