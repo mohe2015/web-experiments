4aa6506c411edcdffda43f66e2cb17673a911241
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-22 21:08:53 +0200

    Test

--- custom-elements.mjs
+++ custom-elements.mjs
@@ -1,35 +1,34 @@
 // https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements
 
-export class Button extends HTMLElement {
-    constructor() {
-        super()
+//  extends HTMLElement
+export function Button() {
+    super()
 
-        var shadow = this.attachShadow({mode: 'open'})
+    var shadow = this.attachShadow({mode: 'open'})
 
-        var button = document.createElement('button')
-        button.setAttribute('type', 'button')
+    var button = document.createElement('button')
+    button.setAttribute('type', 'button')
 
-        var buttonStyle = 'primary' // default
-        buttonStyle = this.hasAttribute('primary') ? 'primary' : buttonStyle;
-        buttonStyle = this.hasAttribute('secondary') ? 'secondary' : buttonStyle;
-        buttonStyle = this.hasAttribute('success') ? 'success' : buttonStyle;
-        buttonStyle = this.hasAttribute('danger') ? 'danger' : buttonStyle;
-        buttonStyle = this.hasAttribute('warning') ? 'warning' : buttonStyle;
-        buttonStyle = this.hasAttribute('info') ? 'info' : buttonStyle;
-        buttonStyle = this.hasAttribute('light') ? 'light' : buttonStyle;
-        buttonStyle = this.hasAttribute('dark') ? 'dark' : buttonStyle;
-        buttonStyle = this.hasAttribute('link') ? 'link' : buttonStyle;
+    var buttonStyle = 'primary' // default
+    buttonStyle = this.hasAttribute('primary') ? 'primary' : buttonStyle;
+    buttonStyle = this.hasAttribute('secondary') ? 'secondary' : buttonStyle;
+    buttonStyle = this.hasAttribute('success') ? 'success' : buttonStyle;
+    buttonStyle = this.hasAttribute('danger') ? 'danger' : buttonStyle;
+    buttonStyle = this.hasAttribute('warning') ? 'warning' : buttonStyle;
+    buttonStyle = this.hasAttribute('info') ? 'info' : buttonStyle;
+    buttonStyle = this.hasAttribute('light') ? 'light' : buttonStyle;
+    buttonStyle = this.hasAttribute('dark') ? 'dark' : buttonStyle;
+    buttonStyle = this.hasAttribute('link') ? 'link' : buttonStyle;
 
-        button.setAttribute('class', `btn btn-${buttonStyle}`)
-        button.textContent = this.textContent;
+    button.setAttribute('class', `btn btn-${buttonStyle}`)
+    button.textContent = this.textContent;
 
-        const linkElem = document.createElement('link');
-        linkElem.setAttribute('rel', 'stylesheet');
-        linkElem.setAttribute('href', '/bootstrap.css');
+    const linkElem = document.createElement('link');
+    linkElem.setAttribute('rel', 'stylesheet');
+    linkElem.setAttribute('href', '/bootstrap.css');
 
-        shadow.appendChild(button);
-        shadow.appendChild(linkElem);
-    }
+    shadow.appendChild(button);
+    shadow.appendChild(linkElem);
 }
 
 customElements.define('my-button', Button)
