c8be0b1d4e1bc9435dde4321fa00dab25f8b4e2c
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-22 22:00:28 +0200

    Implement watching and more logging

--- server.mjs
+++ server.mjs
@@ -5,37 +5,37 @@ const server = http.createServer();
 const wss = new WebSocket.Server({ noServer: true });
 
 wss.on('connection', function connection(ws, request, client) {
-    const ip = request.connection.remoteAddress;
-    console.log('request from ' + ip)
-    ws.on('message', function message(data) {
-      console.log(`Received message ${data} from user ${client}`);
+  console.log('wss.on(\'connection\'')
+  const ip = request.connection.remoteAddress;
+  console.log('connection from ' + ip)
+  ws.on('message', function message(data) {
+    console.log(`Received message ${data} from user ${client.userId}`);
 
-      wss.clients.forEach(function each(client) {
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(data);
-          }
-        });
-
-    });
-});
-
-function authenticate(request, callback) {
-  request.userId = 'test';
-  callback(null, request)
-}
-
-server.on('upgrade', function upgrade(request, socket, head) {
-  authenticate(request, (err, client) => {
-    if (err || !client) {
-      socket.destroy();
-      return;
-    }
-
-    wss.handleUpgrade(request, socket, head, function done(ws) {
-      wss.emit('connection', ws, request/*, client*/);
+    // broadcasting for testing
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
     });
   });
 });
 
-server.listen(1337);
-console.log('listening on ws://localhost:1337')
+async function authenticate(request) {
+  return {
+    userId: 'test'
+  }
+}
+
+server.on('upgrade', async function upgrade(request, socket, head) {
+  console.log('server.on(\'upgrade\'')
+  let client = await authenticate(request);
+  //socket.destroy();
+
+  wss.handleUpgrade(request, socket, head, function done(ws) {
+    console.log('wss.emit(\'connection\'')
+    wss.emit('connection', ws, request, client);
+  });
+});
+
+server.listen(1337)
+console.log('server.listen(1337)')
