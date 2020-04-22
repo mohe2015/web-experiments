bccb419207478a087f50aee77306ee6f13fe9546
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-22 22:36:21 +0200

    Jo

--- database.ts
+++ database.ts
@@ -1,8 +1,4 @@
 // https://developers.google.com/web/ilt/pwa/working-with-indexeddb
-import { openDB, deleteDB, wrap, unwrap } from 'idb';
+import { openDB } from 'idb';
 
-async function setupDatabase() {
-  const db = await openDB('settings', 1)
-}
-
-setupDatabase()
+openDB('settings', 1)
