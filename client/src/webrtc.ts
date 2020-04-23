534ba17abf5dbed5b8f89afe5b27203509007bc9
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-23 19:24:21 +0200

    Checkpoint

--- client/src/webrtc.ts
+++ client/src/webrtc.ts
@@ -14,55 +14,50 @@ export async function makeCall() {
     console.log('new RTCPeerConnection()')
     const peerConnection: RTCPeerConnection = new RTCPeerConnection()
 
-    peerConnection.addEventListener('negotiationneeded', async event => {
-        console.log('peerConnection.addEventListener(\'negotiationneeded\'')
-        const offer = await peerConnection.createOffer()
-        await peerConnection.setLocalDescription(offer)
-        prompt('copy', JSON.stringify(offer))
-
-        let remoteDescription = new RTCSessionDescription(JSON.parse(window.prompt("Remote description: ") as string))
-        await peerConnection.setRemoteDescription(remoteDescription);
+    peerConnection.addEventListener('icecandidate', event => {
+        console.log('peerConnection.addEventListener(\'icecandidate\'' + event)
+        if (event.candidate) {
+            console.log(JSON.stringify(event.candidate))
+        }
     })
 
     peerConnection.addEventListener('connectionstatechange', event => {
         console.log('peerConnection.addEventListener(\'connectionstatechange\'' + peerConnection.connectionState)
     })
 
-    peerConnection.addEventListener('icecandidate', event => {
-        console.log('peerConnection.addEventListener(\'icecandidate\'' + event)
+
+
+    peerConnection.addEventListener('negotiationneeded', async event => {
+        console.log('peerConnection.addEventListener(\'negotiationneeded\'')
+        const offer = await peerConnection.createOffer()
+        await peerConnection.setLocalDescription(offer)
+        console.log('offer', JSON.stringify(offer))
+
+        let input = null;
+        do {
+            input = window.prompt("ICE candidate: ")
+            if (input) {
+                let rtcIceCandidate = new RTCIceCandidate(JSON.parse(input))
+                await peerConnection.addIceCandidate(rtcIceCandidate)
+            }
+        } while (input)
+
+        let remoteDescription = new RTCSessionDescription(JSON.parse(window.prompt("Remote description: ") as string))
+        await peerConnection.setRemoteDescription(remoteDescription);
     })
 
-    var channel: RTCDataChannel = peerConnection.createDataChannel("main")
-    console.log('peerConnection.createDataChannel("main")')
-    channel.onopen = function(event) {
-        console.log('channel.onopen')
-        channel.send('1')
-    }
-    channel.onclose = function(event) {
-        console.log('channel.onclose' + event)
-    }
-    channel.onerror = function(event) {
-        console.log('channel.onerror' + event)
-    }
-    channel.onmessage = function(event) {
-        console.log('channel.onmessage' + event.data)
-    }
+
+    peerConnection.createDataChannel('main'); // this initiates negotiationneeded
 }
 
 export async function receiveCall() {
     const peerConnection = new RTCPeerConnection()
     console.log('new RTCPeerConnection()')
 
-    let remoteDescription = new RTCSessionDescription(JSON.parse(window.prompt("Remote description: ") as string))
-    peerConnection.setRemoteDescription(new RTCSessionDescription(remoteDescription))
-    const answer = await peerConnection.createAnswer()
-    await peerConnection.setLocalDescription(answer)
-    prompt('copy', JSON.stringify(answer))
-
     peerConnection.addEventListener('icecandidate', event => {
         console.log('peerConnection.addEventListener(\'icecandidate\'' + event)
         if (event.candidate) {
-            // TODO send to remote
+            console.log(JSON.stringify(event.candidate))
         }
     })
 
@@ -70,22 +65,25 @@ export async function receiveCall() {
         console.log('peerConnection.addEventListener(\'connectionstatechange\'' + peerConnection.connectionState)
     })
 
-    peerConnection.addEventListener('datachannel', (event: RTCDataChannelEvent) => {
-        console.log('peerConnection.addEventListener(\'datachannel\'')
-        let channel = event.channel
-        channel.onopen = function(event) {
-            console.log('channel.onopen')
-            channel.send('1')
-        }
-        channel.onclose = function(event) {
-            console.log('channel.onclose' + event)
-        }
-        channel.onerror = function(event) {
-            console.log('channel.onerror' + event)
-        }
-        channel.onmessage = function(event) {
-            console.log('channel.onmessage' + event.data)
-        }
+
+
+    peerConnection.addEventListener('negotiationneeded', async event => {
+        let remoteDescription = new RTCSessionDescription(JSON.parse(window.prompt("Remote description: ") as string))
+        peerConnection.setRemoteDescription(new RTCSessionDescription(remoteDescription))
+        const answer = await peerConnection.createAnswer()
+        await peerConnection.setLocalDescription(answer)
+
+        let input = null;
+        do {
+            input = window.prompt("ICE candidate: ")
+            if (input) {
+                let rtcIceCandidate = new RTCIceCandidate(JSON.parse(input))
+                await peerConnection.addIceCandidate(rtcIceCandidate)
+            }
+        } while (input)
+
+        console.log('answer', JSON.stringify(answer))
     })
 
+    peerConnection.createDataChannel('main'); // this initiates negotiationneeded
 }
