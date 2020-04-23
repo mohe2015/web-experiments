2218e258b029dca7279ea34fe362df0e254f724b
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-23 17:46:37 +0200

    Update

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -2,10 +2,14 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from "@rollup/plugin-node-resolve";
 
 export default {
-  input: './src/database.ts',
+  input: [
+    './src/database.ts',
+    './src/custom-elements.ts',
+    './src/webrtc.ts',
+    './src/index.ts'
+  ],
   output: {
-    name: 'index',
-    dir: 'output',
+    dir: 'build',
     format: 'es',
     sourcemap: true
   },
