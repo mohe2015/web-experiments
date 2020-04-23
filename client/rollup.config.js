31dff225681dd248d61f4453cf7d08ef2418ab8b
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-23 17:28:08 +0200

    Jo

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -2,15 +2,13 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from "@rollup/plugin-node-resolve";
 
 export default {
-  input: 
-    './src/database.ts'
-  ,
+  input: './src/database.ts',
   output: {
     dir: 'output',
     format: 'es'
   },
   plugins: [
-    resolve({ jsnext: true }),
+    resolve({ jsnext: true, extensions: [".ts"] }),
     typescript(),
   ]
 };
