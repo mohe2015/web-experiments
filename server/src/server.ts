d389b75bb7df91be7043f448d7f5791377328a31
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-23 17:57:32 +0200

    Jo

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,41 +1,21 @@
-import http from 'http'
-import WebSocket from 'ws'
+import { createServer, Server, IncomingMessage } from 'http'
+import * as WebSocket from 'ws'
 
-const server: http.Server = http.createServer();
-const wss = new WebSocket.Server({ noServer: true });
+const server: Server = createServer();
+const wss: WebSocket.Server = new WebSocket.Server({ server: server });
 
-wss.on('connection', function connection(ws, request, client) {
+wss.on('connection', function (socket: WebSocket, request: IncomingMessage) {
   console.log('wss.on(\'connection\'')
   const ip = request.connection.remoteAddress;
   console.log('connection from ' + ip)
-  ws.on('message', function message(data) {
-    console.log(`Received message ${data} from user ${client.userId}`);
+  socket.on('message', function message(data) {
+    console.log(`Received message ${data} from user`);
 
     // broadcasting for testing
     wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
+      if (client !== socket && client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
     });
   });
 });
-
-async function authenticate(request) {
-  return {
-    userId: 'test'
-  }
-}
-
-server.on('upgrade', async function upgrade(request, socket, head) {
-  console.log('server.on(\'upgrade\'')
-  let client = await authenticate(request);
-  //socket.destroy();
-
-  wss.handleUpgrade(request, socket, head, function done(ws) {
-    console.log('wss.emit(\'connection\'')
-    wss.emit('connection', ws, request, client);
-  });
-});
-
-server.listen(1337)
-console.log('server.listen(1337)')
