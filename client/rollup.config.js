2ee4efdee91c49d84d85a7f72e06af4dc58b58e9
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-23 17:38:54 +0200

    Clean up

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -4,12 +4,14 @@ import resolve from "@rollup/plugin-node-resolve";
 export default {
   input: './src/database.ts',
   output: {
+    name: 'index',
     dir: 'output',
     format: 'es',
     sourcemap: true
   },
   plugins: [
     resolve({ jsnext: true, extensions: [".ts"] }),
-    typescript(),
+    typescript({
+    }),
   ]
 };
--- client/src/database.js
+++
@@ -1,8 +0,0 @@
-// https://developers.google.com/web/ilt/pwa/working-with-indexeddb
-import * as idb from 'idb';
-async function test() {
-    let db1 = await idb.openDB('settings', 1);
-    db1.close();
-}
-test();
-//# sourceMappingURL=database.js.map
