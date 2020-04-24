f8c3ad67c97f6c2cf22e1d14c66939afacc31b3d
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-24 20:03:24 +0200

    Clean up

--- client/src/asymmetric-webcryptography.ts
+++ client/src/asymmetric-webcryptography.ts
@@ -1,12 +1,5 @@
 import { setQRCodeData } from './qrcode-generator'
 
-function getMessageEncoding1() {
-  const messageBox = document.querySelector(".ecdsa #message") as HTMLInputElement;
-  let message = messageBox.value;
-  let enc = new TextEncoder();
-  return enc.encode(message);
-}
-
 async function main() {
   let keyPair = await window.crypto.subtle.generateKey(
     {
@@ -17,22 +10,12 @@ async function main() {
     ["sign", "verify"]
   );
 
-  let encoded = getMessageEncoding1();
-  let signature = await window.crypto.subtle.sign(
-    {
-      name: "ECDSA",
-      hash: {name: "SHA-384"},
-    },
-    keyPair.privateKey,
-    encoded
-  );
-  console.log(signature)
-
   let test = await window.crypto.subtle.exportKey('spki', keyPair.publicKey)
 
   let test1 = btoa(String.fromCharCode(...new Uint8Array(test)))
 
- setQRCodeData("https://moritz.local/#"+test1)
+  setQRCodeData("https://moritz.local/#"+test1)
+  
 }
 
 main()
