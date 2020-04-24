d4c012a08df7f92a8334de34322c65c2a7664d4c
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-24 23:27:27 +0200

    This lib technique doesn't work currently

--- client/src/urlhash-receiver.ts
+++ client/src/urlhash-receiver.ts
@@ -1,16 +1,10 @@
+import { importAsyncPublicKey } from '../../lib/src/asymmetric-webcryptography'
+
 async function testttt() {
   if (window.location.hash) {
     let test = Uint8Array.from(atob(window.location.hash.substr(1)), c => c.charCodeAt(0))
     console.log("bbb" + test)
-    // @ts-ignore
-    let key = await window.crypto.subtle.importKey("raw", test,
-      {
-        name: "ECDSA",
-        namedCurve: "P-521"
-      } as EcKeyImportParams,
-      true,
-      ["verify"]
-    )
+    let key = importAsyncPublicKey(test)
     console.log(key)
   }
 }
--- lib/src/asymmetric-webcryptography.ts
+++ lib/src/asymmetric-webcryptography.ts
@@ -1,5 +1,5 @@
-async function main() {
-  let keyPair = await window.crypto.subtle.generateKey(
+export async function createAsymmetricKeyPair() {
+  return await window.crypto.subtle.generateKey(
     {
       name: "ECDSA",
       namedCurve: "P-521"
@@ -7,15 +7,50 @@ async function main() {
     true,
     ["sign", "verify"]
   );
-
-  let test = await window.crypto.subtle.exportKey('raw', keyPair.publicKey)
-
-  console.log("a")
-  console.log(new Uint8Array(test))
-
-  let test1 = btoa(String.fromCharCode(...new Uint8Array(test)))
-  
-  console.log(test1)
 }
 
-main()
+export async function storeNewAsymmetricKeyPair() {
+  let keyPair: CryptoKeyPair = await createAsymmetricKeyPair();
+  let exportedPublicKey = await window.crypto.subtle.exportKey("jwk", keyPair.publicKey)
+  let exportedPrivateKey = await window.crypto.subtle.exportKey("jwk", keyPair.privateKey)
+  window.localStorage.setItem("publicSigningKey", JSON.stringify(exportedPublicKey))
+  window.localStorage.setItem("privateSigningKey", JSON.stringify(exportedPrivateKey))
+}
+
+export async function importAsyncPublicKey(test: Uint8Array) {
+  return await window.crypto.subtle.importKey("jwk", test,
+    {
+      name: "ECDSA",
+      namedCurve: "P-521"
+    } as EcKeyImportParams,
+    true,
+    ["verify"]
+  )
+}
+
+export async function importAsyncPrivateKey(test: Uint8Array) {
+  return await window.crypto.subtle.importKey("jwk", test,
+    {
+      name: "ECDSA",
+      namedCurve: "P-521"
+    } as EcKeyImportParams,
+    true,
+    ["sign"]
+  )
+}
+
+export async function getAsymmetricPublicKey() {
+  let storedKey = window.localStorage.getItem("publicSigningKey")
+  if (!storedKey) {
+    storeNewAsymmetricKeyPair()
+  }
+  return await importAsyncPublicKey(JSON.parse(window.localStorage.getItem("publicSigningKey") as string))
+}
+
+export async function getAsymmetricPrivateKey() {
+  let storedKey = window.localStorage.getItem("privateSigningKey")
+  if (!storedKey) {
+    storeNewAsymmetricKeyPair()
+  }
+  return await importAsyncPrivateKey(JSON.parse(window.localStorage.getItem("privateSigningKey") as string))
+}
