9d279953ac825a0e0ba568b4c4b204bf373b3f8e
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-25 23:42:33 +0200

    Ideas

---
+++ src/client/editing.ts
@@ -0,0 +1,11 @@
+// I think block editing that's not 100% wysiwyg is better than an arbitrary editor
+// easier to convert and not so much unexpected things
+// also more user friendly?
+// possible to create page builder
+
+// yjs
+
+// just get inspired by them
+// maybe custom elements may help or so
+// editor.js
+// gutenberg
