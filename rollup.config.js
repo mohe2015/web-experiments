34d7a677a5eedda1da1784288ba27e019c79b7a6
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-26 22:38:31 +0200

    Fuck YOU TYPESCRIPT

--- rollup.config.js
+++ rollup.config.js
@@ -3,28 +3,17 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: [
-    './src/client/database.ts',
-    './src/client/custom-elements.ts',
-    './src/client/webrtc.ts',
-    './src/client/index.ts',
-    './src/client/symmetric-webcryptography.ts',
-    './src/client/urlhash-receiver.ts',
-    './src/lib/asymmetric-webcryptography.ts'
-  ],
+  input: './src/client/editing.js',
   output: {
     dir: 'build',
     format: 'esm',
     sourcemap: true
   },
   plugins: [
-    typescript(),
     commonjs(),
     resolve({
-      preferBuiltins: false,
-      browser: true, 
-      jsnext: true, 
-      extensions: [".ts", ".js"]
+      mainFields: ['browser', 'module', 'main'],
+      extensions: [".mjs", ".js", ".ts"]
     }),
   ],
 };
