6a6cfb41eee0d8a1e6c7f79ea78e77fd08ae8c42
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-26 20:59:53 +0200

    Update

--- src/client/editing.ts
+++ src/client/editing.ts
@@ -1,23 +1,15 @@
 // I think block editing that's not 100% wysiwyg is better than an arbitrary editor
-// easier to convert and not so much unexpected things
-// also more user friendly?
-// possible to create page builder
-// yjs
-// just get inspired by them
-// maybe custom elements may help or so
-// editor.js
-// gutenberg
-// USE https://en.wikipedia.org/wiki/Conflict-free_replicated_data_type
-
+// maybe custom elements may help or so, editor.js
 // node node_modules/y-webrtc/bin/server.js
-
 import * as Y from 'yjs'
 import { WebrtcProvider } from 'y-webrtc'
 
 const ydoc = new Y.Doc()
-// clients connected to the same room-name share document updates
-const provider = new WebrtcProvider('your-room-name', ydoc)
-const yarray = ydoc.get('array', Y.Array)
+new WebrtcProvider('your-room-name', ydoc)
+const yarray = ydoc.getArray('array')
+// TODO update first
 
-console.log(provider)
 console.log(yarray)
+yarray.push(["a", "b", "c"])
+
+yarray.forEach(e => console.log(e))
